81b1206a9279a935237ab2dc9426cb08cc120dbc
arroyo-piedra <40010920+arroyo-piedra@users.noreply.github.com>
2019-03-28 14:43:49 -0600

    DTO
    La clase DTO ya fue completada para la primera iteración

--- Gestor-Actividades/DTO/DTO.cs
+++ Gestor-Actividades/DTO/DTO.cs
@@ -7,7 +7,37 @@ namespace Gestor_Actividades.DTO
 {
     public class DTO
     {
-        private String Usuario;
-        private String C
-    }
+        //usuario
+        private String UsuarioLogIng { get; set; }
+        private String ContraseñaLogIn { get; set; }
+
+        //actividades
+        private DateTime ActividadFecha { get; set; }
+        private String ActividadNomhbre { get; set; }
+        private String ActividadHorario { get; set; }
+        private String ActividadCampus { get; set; }
+        private bool ActividadRestriccion { get; set; }
+        private String ActividadEncargado { get; set; }
+
+        //eventos
+        private String EventoNombre { get; set; }
+        private DateTime EventoFecha { get; set; }
+        private String EventoExpositor { get; set; }
+
+        //staff
+        private String StaffNombre { get; set; }
+        private String StaffUsuario { get; set; }
+        private String StaffContraseña { get; set; }
+
+        //archivos
+        private String ArchivoPath { get; set; }
+
+        }
+
+
+
+        public DTO()
+        {
+
+        }
 }
