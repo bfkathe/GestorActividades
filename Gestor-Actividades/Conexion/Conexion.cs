f6b345a51b2edd7c49671e53e551016c7e066f85
arroyo-piedra <40010920+arroyo-piedra@users.noreply.github.com>
2019-03-28 14:19:11 -0600

    DTO
    Dto agregado

---
+++ Gestor-Actividades/Conexion/Conexion.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace Gestor_Actividades.Conexion
+{
+    public class Conexion
+    {
+        private string cadena = "Data Source=ANDRE\\SQLEXPRESS ; Initial Catalog=ProyectoGestorActividades; Integrated Security=True";
+        public SqlConnection conn = new SqlConnection();
+
+        public Conexion()
+        {
+            conn.ConnectionString = cadena;
+            System.Diagnostics.Debug.WriteLine("Conexion abierta");
+        }
+
+        public void Abrir()
+        {
+            try
+            {
+                conn.Open();
+                System.Diagnostics.Debug.WriteLine("Conexion abierta");
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine("Conexion fallida" + ex.Message);
+            }
+        }
+
+        public void cerrar()
+        {
+            conn.Close();
+        }
+
+        /*public void agregarNombre(string nombre, int edad)
+        {
+            SqlCommand cmd = new SqlCommand("insNombre", conn);
+            cmd.CommandType = CommandType.StoredProcedure;
+            try
+            {
+                cmd.Parameters.Add("@nombre",SqlDbType
+                    .NChar).Value = nombre;
+                cmd.Parameters.Add("@edad", SqlDbType.Int).Value = edad;
+                Abrir();
+                cmd.ExecuteNonQuery();
+                cerrar();
+            }
+            catch
+            {
+                throw new System.Exception("Error insertando en la base");
+            }
+        }*/
+    }
+}
---
+++ Gestor-Actividades/DTO/DTO.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Gestor_Actividades.DTO
+{
+    public class DTO
+    {
+    }
+}
