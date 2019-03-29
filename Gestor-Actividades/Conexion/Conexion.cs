a1ba5547beb7ff9d9d1ef61c9b193d717c9701fb
audrarg <audrarg@hotmail.com>
2019-03-28 20:02:30 -0600

    Cambios

---
+++ Gestor-Actividades/Conexion/Conexion.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace Gestor_Actividades.Conexion
+{
+    public class Conexion
+    {
+        private string cadena = "Data Source=DESKTOP-7K75JTA\\SQLEXPRESS ; Initial Catalog=prueba; Integrated Security=True";
+        public SqlConnection conn = new SqlConnection();
+
+        public Conexion()
+        {
+            conn.ConnectionString = cadena;
+            }
+
+        public void Abrir()
+        {
+            try
+            {
+                conn.Open();
+                System.Diagnostics.Debug.WriteLine("Conexion abierta");
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine("Conexion fallida" + ex.Message);
+            }
+        }
+
+        public void cerrar()
+        {
+            conn.Close();
+        }
+
+        public void agregarNombre(string nombre, int edad)
+        {
+            SqlCommand cmd = new SqlCommand("insNombre", conn);
+            cmd.CommandType = CommandType.StoredProcedure;
+            try
+            {
+                cmd.Parameters.Add("@nombre",SqlDbType
+                    .NChar).Value = nombre;
+                cmd.Parameters.Add("@edad", SqlDbType.Int).Value = edad;
+                Abrir();
+                cmd.ExecuteNonQuery();
+                cerrar();
+            }
+            catch
+            {
+                throw new System.Exception("Error insertando en la base");
+            }
+        }
+    }
+}
---
+++ Gestor-Actividades/Controlador/Controlador.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Gestor_Actividades.Conexion;
+
+namespace Gestor_Actividades.Controlador
+{
+    public class Controlador
+    {
+        public Conexion.Conexion conexion = new Conexion.Conexion();
+    }
+}
