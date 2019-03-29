a3a4cbe964ef18a261494088aa82f1630d1077fa
arroyo-piedra <40010920+arroyo-piedra@users.noreply.github.com>
2019-03-29 12:17:03 -0600

    Modelo
    Se agregaron las clases de la parte de modelo

---
+++ Gestor-Actividades/Modelo/Archivo.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Gestor_Actividades.Modelo
+{
+    public class Archivo
+    {
+
+        private String nombre;
+        private String formato;
+        private String ruta;
+
+        public Archivo()
+        {
+
+        }
+
+        public Archivo(string nom, string form, string path)
+        {
+            this.nombre = nom;
+            this.formato = form;
+            this.ruta = path;
+        }
+
+        public String getNombre()
+        {
+            return this.nombre;
+        }
+
+        public String getFormato()
+        {
+            return this.formato;
+        }
+
+        public String getRuta()
+        {
+            return this.ruta;
+        }
+
+        public void setNombre(String nom)
+        {
+            this.nombre = nom;
+        }
+
+        public void setFormato(String form)
+        {
+            this.formato = form;
+        }
+
+        public void setRuta(String rut)
+        {
+            this.ruta = rut;
+        }
+
+    }
+}
---
+++ Gestor-Actividades/Modelo/Staff.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Gestor_Actividades.Modelo
+{
+    public class Staff
+    {
+        private String nombre;
+        private String usuario;
+        private String contraseña;
+
+        public Staff()
+        {
+
+        }
+
+        public Staff(string nom, string usu, string contra)
+        {
+            this.nombre = nom;
+            this.usuario = usu;
+            this.contraseña = contra;
+        }
+
+        public String getNombre()
+        {
+            return this.nombre;
+        }
+
+        public String getUsuario()
+        {
+            return this.usuario;
+        }
+
+        public String getContraseña()
+        {
+            return this.contraseña;
+        }
+
+        public void setNombre(String nom)
+        {
+            this.nombre = nom;
+        }
+
+        public void setUsuario(String usu)
+        {
+            this.usuario = usu;
+        }
+
+        public void setContraseña(String contra)
+        {
+            this.contraseña = contra;
+        }
+
+    }
+}
