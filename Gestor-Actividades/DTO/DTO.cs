796389e3571b05d0d3e4cd63feb41ffc899253f0
audrarg <audrarg@hotmail.com>
2019-03-28 20:25:12 -0600

    Commit

---
+++ Gestor-Actividades/DTO/DTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Gestor_Actividades.DTO
+{
+    public class DTO
+    {
+        private String Usuario;
+        private String C
+    }
+}
