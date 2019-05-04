dc7dfd67df27a5e79ffeb767196ca8f9a9f32eae
arroyo-piedra <40010920+arroyo-piedra@users.noreply.github.com>
2019-05-03 21:11:43 -0600

    Base

--- SQL/tablasIteracion2.sql
+++ SQL/tablasIteracion2.sql
@@ -1,35 +1,26 @@
 ----tablas de la segunda iteración----
 use ProyectoGestorActividades
 
-create table Participantes(
-	ParticipanteId int identity(1,1) primary key,
-	Apellido1 varchar(25) not null,
-	Apellido2 varchar(25) not null,
-	Nombre varchar(25) not null,
-	Identidicacion int not null,
-	Correo nvarchar(255) not null
-);
-
-select * from Participantes
-
-
 create table Curso(
 	CursoId int identity(1,1) primary key,
 	Nombre nvarchar(255) not null,
 	Numero int
 );
 
-
 create table ParticipantesxActividad(
 	ParticipanteActividadId int identity(1,1) primary key,
-	ParticipanteId int not null,
 	ActividadId int not null,
-	Asistencia bit not null, ---0 No, 1 Sí
+	Asistencia bit not null default 0, ---0 No, 1 Sí
 	CursoId int,
-	NumeroCurso int,
 );
 
+
 alter table ParticipantesxActividad add tipoParticipanteId int
+alter table ParticipantesxActividad add Apellido1 varchar(25)
+alter table ParticipantesxActividad add Apellido2 varchar(25)
+alter table ParticipantesxActividad add Nombre varchar(25)
+alter table ParticipantesxActividad add Identificacion int
+alter table ParticipantesxActividad add Correo varchar(255)
 
 create table TipoParticipante(
 	TipoId int identity(1,1) primary key,
@@ -48,11 +39,6 @@ foreign key (ActividadId) references Actividades(ActividadId);
 alter table ParticipantesxActividad
 add constraint FK_CursoPA
 foreign key (CursoId) references Curso(CursoId);
-
---Llave de participante en participantes por actividad
-alter table ParticipantesxActividad
-add constraint FK_ParticipantePA
-foreign key (ParticipanteId) references Participantes(ParticipanteId);
 
 --Llave de tipo en participantes por actividad
 alter table ParticipantesxActividad
